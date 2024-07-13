7f97072284646dbe3e6555af2b3a07f116b3aefe
Justin Young <justin_dean_young@yahoo.com>
2024-07-12 20:19:46 -0400

    created homepage component

---
+++ src/components/home.ts
@@ -0,0 +1,25 @@
+export class HomePage {
+  template = `
+    <div>
+    <h1>Home</h1>
+      <p>This demo application is made to demonstrate the capabilities of Peasy-UI.  One aspect of peasy is that it can emulate a Single Page Application, and completely control the content of your app.  This demo is a mock-up of a SPA using Peasy-UI.
+      </p>  
+
+      <p>You can click the navbar links or follow these links below.</p>
+
+      <div>
+        <li><a class="home_link" href="#peasy">What is Peasy-UI</a></li>
+        <li><a class="home_link" href="#bindings">Templates and Bindings</a></li>
+        <li><a class="home_link" href="#events">Events</a></li>
+        <li><a class="home_link" href="#css">CSS</a></li>
+        <li><a class="home_link" href="#animations">Animations</a></li>
+        <li><a class="home_link" href="#components">Components</a></li>
+        <li><a class="home_link" href="#about">About Me</a></li>
+      </div>
+    </div>
+    
+    </div>
+    `;
+
+  constructor() {}
+}
--- src/components/router.ts
+++ src/components/router.ts
@@ -1,5 +1,9 @@
 type Routes = "home" | "peasy" | "bindings" | "events" | "css" | "animations" | "components" | "about";
 
+import { HomePage } from "./home";
+
+const homepage = new HomePage();
+
 export class Router {
   current: Routes = "home";
 
@@ -55,21 +59,7 @@ export class Router {
   </style>
   <div class="router">
     <div \${===router.showHome}>
-      <h1>Home</h1>
-      <p>This demo application is made to demonstrate the capabilities of Peasy-UI.  One aspect of peasy is that it can emulate a Single Page Application, and completely control the content of your app.  This demo is a mock-up of a SPA using Peasy-UI.
-      </p>  
-
-      <p>You can click the navbar links or follow these links below.</p>
-
-      <div>
-        <li><a class="home_link" href="#peasy">What is Peasy-UI</a></li>
-        <li><a class="home_link" href="#bindings">Templates and Bindings</a></li>
-        <li><a class="home_link" href="#events">Events</a></li>
-        <li><a class="home_link" href="#css">CSS</a></li>
-        <li><a class="home_link" href="#animations">Animations</a></li>
-        <li><a class="home_link" href="#components">Components</a></li>
-        <li><a class="home_link" href="#about">About Me</a></li>
-      </div>
+        ${homepage.template}
     </div>
     <div \${===router.showPeasy}>What  is Peasy-UI</div>
     <div \${===router.showBindings}>Templates and Bindings</div>
